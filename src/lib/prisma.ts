6fcb2cdbe739b981a2f14f5d97a53176d2461f71
patoigle20 <33643695+patoigle20@users.noreply.github.com>
2025-11-08 11:42:39 -0300

    Rename global Prisma client variable

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,13 +1,13 @@
-// Prisma client singleton para Next.js
+// Prisma client singleton para Next.js / server
 import { PrismaClient } from '@prisma/client';
 
 declare global {
   // eslint-disable-next-line no-var
-  var prisma: PrismaClient | undefined;
+  var __prisma: PrismaClient | undefined;
 }
 
-const prisma = global.prisma || new PrismaClient();
+const prisma = global.__prisma || new PrismaClient();
 
-if (process.env.NODE_ENV !== 'production') global.prisma = prisma;
+if (process.env.NODE_ENV !== 'production') global.__prisma = prisma;
 
 export default prisma;
